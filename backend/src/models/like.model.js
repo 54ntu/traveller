75aa18487a954e42641d1ff229f48f361aac081c
54ntu <chaudharysantaram8@gmail.com>
2025-01-07 20:28:25 +0545

    like model is created

---
+++ backend/src/models/like.model.js
@@ -0,0 +1,18 @@
+import mongoose, { mongo } from "mongoose";
+
+const likeSchema = new mongoose.Schema(
+  {
+    post: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
+    likedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+const Like = mongoose.model("Like", likeSchema);
+export default Like;
