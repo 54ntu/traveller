27f92dec3d3ce0ab5fd68c963ae982cda9cfea81
54ntu <chaudharysantaram8@gmail.com>
2025-01-11 17:41:38 +0545

    getallpost and post updation controller is created and tested successfully....git add .!

--- backend/src/controllers/post.controllers.js
+++ backend/src/controllers/post.controllers.js
@@ -1,4 +1,5 @@
 import Post from "../models/post.models.js";
+import ApiResponse from "../services/ApiResponse.js";
 
 class PostController {
   static async addpost(req, res) {
@@ -24,7 +25,70 @@ class PostController {
       owner: userid,
     });
 
-    console.log(postdata);
+    const ispostcreated = await Post.findById(postdata._id);
+    if (!ispostcreated) {
+      return res.status(500).json({
+        message: "post creation failed.!!",
+      });
+    }
+
+    return res
+      .status(201)
+      .json(
+        new ApiResponse(
+          201,
+          ispostcreated,
+          "post created successsfully😎😎😎😎😎"
+        )
+      );
+  }
+
+  static async getAllPost(req, res) {
+    const posts = await Post.find();
+    if (posts.length === 0) {
+      return res.status(404).json({
+        message: "posts not found...!!!",
+      });
+    }
+
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, posts, "post data fetched successfully.😊😊😊😊")
+      );
+  }
+
+  static async updatePost(req, res) {
+    const userid = req.user.id;
+    // console.log(`user id is ${userid}`);
+    const { id } = req.params;
+    // console.log(id);
+    const { content } = req.body;
+    const photourl = req.files?.photos?.[0]?.filename;
+    const videourl = req.files?.videos?.[0]?.filename;
+    const updatedPost = await Post.findOneAndUpdate(
+      { _id: id, owner: userid },
+      {
+        content,
+        photos: photourl,
+        videos: videourl,
+      },
+      {
+        new: true,
+      }
+    );
+    // console.log(updatedPost);
+    if (!updatedPost) {
+      return res.status(500).json({
+        message: "post updation failed.!",
+      });
+    }
+
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, updatedPost, "post updated successfully.😊😊😊😊")
+      );
   }
 }
 
--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -4,13 +4,25 @@ import PostController from "../controllers/post.controllers.js";
 import UserMiddleware from "../middleware/user.middleware.js";
 const router = express.Router();
 
-router.route("/").post(
+router
+  .route("/")
+  .post(
+    UserMiddleware.isUserLoggedIn,
+    upload.fields([
+      { name: "video", maxCount: 1 },
+      { name: "photo", maxCount: 1 },
+    ]),
+    PostController.addpost
+  )
+  .get(PostController.getAllPost);
+
+router.route("/:id").patch(
   UserMiddleware.isUserLoggedIn,
   upload.fields([
     { name: "video", maxCount: 1 },
     { name: "photo", maxCount: 1 },
   ]),
-  PostController.addpost
+  PostController.updatePost
 );
 
 export default router;
